6a5f093e92662ffe94820a2a1c755859278a1d39
hacker <hack@gmail.com>
2020-10-07 18:47:30 +0300

    1.2-7

--- sysinfo.sh
+++ sysinfo.sh
@@ -1,32 +1,36 @@
-OS=$(uname)
-KERNEL=$(uname -r)
-FULLNAMEOS=$(uname -o)
-NETNAME=$(uname -n)
-ARCH=$(arch)
-IP=$(curl ifconfig.me)
-IPFORWARDED=$(curl ifconfig.me/forwarded)
-WHOAMI=$(whoami)
-clear
-echo "Coded by t.me/Andreyoss"
-echo "Group - t.me/hak_team"
-echo "Channel - t.me/termuxhub"
-echo ""
-echo ""
-echo ""
-echo "System Information"
-echo ""
-echo "OS - $OS"
-echo "Full Name Of OS - $FULLNAMEOS"
-echo "Kernel - $KERNEL"
-echo "Network Name Machine - $NETNAME"
-echo "Architecture - $ARCH"
-echo ""
-echo "Network Information"
-echo ""
-echo "IP - $IP"
-echo "IP Forwarded - $IPFORWARDED"
-echo ""
-echo "Users Information"
-echo ""
-echo "Current User - $WHOAMI"
-echo ""
+if ping -q -c 1 -W 1 8.8.8.8 > /dev/null 2>&1; then
+  OS=$(uname)
+  KERNEL=$(uname -r)
+  FULLNAMEOS=$(uname -o)
+  NETNAME=$(uname -n)
+  ARCH=$(arch)
+  IP=$(curl ifconfig.me > s)
+  IPFORWARDED=$(curl ifconfig.me/forwarded)
+  WHOAMI=$(whoami)
+  clear
+  echo "Coded by t.me/Andreyoss"
+  echo "Group - t.me/hak_team"
+  echo "Channel - t.me/termuxhub"
+  echo ""
+  echo ""
+  echo ""
+  echo "System Information"
+  echo ""
+  echo "OS - $OS"
+  echo "Full Name Of OS - $FULLNAMEOS"
+  echo "Kernel - $KERNEL"
+  echo "Network Name Machine - $NETNAME"
+  echo "Architecture - $ARCH"
+  echo ""
+  echo "Network Information"
+  echo ""
+  echo "IP - $IP"
+  echo "IP Forwarded - $IPFORWARDED"
+  echo ""
+  echo "Users Information"
+  echo ""
+  echo "Current User - $WHOAMI"
+  echo ""
+else
+  echo "Network Unreachable! Cannot start script"
+fi
